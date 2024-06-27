8ea45e6abe3d2b53ead73a22ddd9f76825319b21
hphuc <lhphuc12102003@gmail.com>
2024-06-27 16:51:34 +0700

    coupon-phuc 27.06.2024

--- project/src/component/home/HomePage.jsx
+++ project/src/component/home/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../layouts/Header";
 import "./style.css";
 import FooterWithSocialLinks from "../../layouts/Footer";
@@ -6,10 +6,58 @@ import { Carousel, Image } from "antd";
 import Bg1 from "../../assets/images/bg_tradition2.png";
 import Bg2 from "../../assets/images/bg_tradition.png";
 import Bg3 from "../../assets/images/xe2.jpg";
+import { couponView } from "../../service/CouponService";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
+  Button,
+  Input,
+  Alert,
+} from "@material-tailwind/react";
+import * as UserService from "../../service/UserService";
+
 function HomePage() {
 
   const [visible, setVisible] = useState(false);
-  
+  const [coupons, setCoupons] = useState();
+  const [change, setChange] = useState(false);
+  const [profile, setProfile] = useState({});
+  useEffect(() => {
+    fetchProfileInfo();
+    fetchCoupons();
+    console.log(profile);
+  }, [change]);
+
+  const fetchProfileInfo = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await UserService.getYourProfile(token);
+      setProfile(response.account);
+    } catch (error) {
+      console.error("Error fetching profile information:", error);
+    }
+  };
+
+  const fetchCoupons = async () => {
+    try {
+      const response = await couponView();
+      setCoupons(response);
+    } catch (error) {
+      console.error('Error fetching profile information:', error);
+    }
+  };
+
+  const getCoupon = async (couponId) => {
+    try {
+      // await setCouponCustomer(couponId, profile.accountId);
+      setChange(!change);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <Header />
@@ -49,6 +97,93 @@ function HomePage() {
           <Image src={Bg3}/>
         </Image.PreviewGroup>
       </div>
+
+      {/* display coupon card */}
+      {coupons >= 1 && (
+        <Card>
+        <CardHeader variant="gradient" color="gray" className="mb-8 p-6">
+          <Typography variant="h6" color="white">
+            COUPONS
+          </Typography>
+        </CardHeader>
+        <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
+          <table className="w-full min-w-[640px] table-auto">
+            <thead>
+              <tr>
+                {["coupon name", "coupon value", "quantity", ""].map((el) => (
+                  <th
+                    key={el}
+                    className="border-b border-blue-gray-50 py-3 px-5 text-left"
+                  >
+                    <Typography
+                      variant="small"
+                      className="text-[11px] font-bold uppercase text-blue-gray-400"
+                    >
+                      {el}
+                    </Typography>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {coupons?.map(
+                ({ couponId, couponName, couponValue, couponQuantity }, key) => {
+                  const className = `py-3 px-5 ${
+                    key === coupons.length - 1
+                      ? ""
+                      : "border-b border-blue-gray-50"
+                  }`;
+
+                  return (
+                    <tr key={couponId}>
+                      <td className={className}>
+                        <div className="flex items-center gap-4">
+                          <div>
+                            <Typography className="text-xs font-normal text-blue-gray-500">
+                              {couponName}
+                            </Typography>
+                          </div>
+                        </div>
+                      </td>
+                      <td className={className}>
+                        <div className="flex items-center gap-4">
+                          <div>
+                            <Typography className="text-xs font-normal text-blue-gray-500">
+                              {couponValue}
+                            </Typography>
+                          </div>
+                        </div>
+                      </td>
+                      <td className={className}>
+                        <div className="flex items-center gap-4">
+                          <div>
+                            <Typography className="text-xs font-normal text-blue-gray-500">
+                              {couponQuantity}
+                            </Typography>
+                          </div>
+                        </div>
+                      </td>
+                      
+                      <td className={className}>
+                        <Typography
+                          as="a"
+                          className="text-xs font-semibold text-red-600"
+                          onClick={() => getCoupon(couponId)}
+                        >
+                          Get
+                        </Typography>
+                      </td>
+                    </tr>
+                  );
+                }
+              )}
+            </tbody>
+          </table>
+        </CardBody>
+      </Card>
+      )}
+      {/*  */}
+
       <section className="bg-blue-600 text-white py-20">
         <div className="container mx-auto text-center">
           <h2 className="text-5xl font-bold mb-5">Book a Driver Easily</h2>
