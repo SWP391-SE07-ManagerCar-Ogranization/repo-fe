41e6552dd0ab6c4e109fcbb8d9d2358d18583ba3
Minh Nguyen <draculedolar0763@gmail.com>
2024-07-06 14:17:10 +0700

    merge fe dev + fix ProtectedRoutes

--- project/src/routes/ProtectedRoutes.js
+++ project/src/routes/ProtectedRoutes.js
@@ -6,18 +6,13 @@ export default function ProtectedRoute({ children, roles }) {
   const navigate = useNavigate();
   const role = localStorage.getItem("role");
   useEffect(() => {
-<<<<<<< HEAD
-    // if(isTokenExpire()) {
-    //   logout();
-    //   navigate("/login", { replace: true });
-    // }
-=======
+
     if(isTokenExpire()) {
       logout();
       navigate("/login", { replace: true });
     }
     console.log(isTokenExpire());
->>>>>>> origin/dev
+
     if (role === null) {
       navigate("/login", { replace: true });
     } else if (!roles.includes(role)) {
