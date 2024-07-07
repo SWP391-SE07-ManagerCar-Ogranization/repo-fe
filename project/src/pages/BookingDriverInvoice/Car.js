6f7515e5f2ecb466d9d559b0c4a0277a05467110
PC <muoivo.051064@gmail.com>
2024-07-08 06:11:23 +0700

    update merge booking

--- project/src/pages/BookingDriverInvoice/Car.js
+++ project/src/pages/BookingDriverInvoice/Car.js
@@ -14,6 +14,10 @@ import { IoIosCloseCircle } from "react-icons/io";
 import LeafletGeocoder from "../../component/layouts/Map/LeafletGeocoder";
 import LeafletRoutingMachine from "../../component/layouts/Map/LeafletRoutingMachine";
 import { addInvoiceAndTransaction } from "../../service/TransactionService";
+import { GeoSearchControl, OpenStreetMapProvider } from "leaflet-geosearch";
+
+import { Popover, Button } from "antd";
+import { toast } from "react-toastify";
 
 const UpdateMapCenter = ({ position }) => {
   const map = useMap();
@@ -22,6 +26,11 @@ const UpdateMapCenter = ({ position }) => {
 };
 
 const BookingTraditional = () => {
+  const [suggestions, setSuggestions] = useState([]);
+  const provider = new OpenStreetMapProvider();
+  const [timeoutId, setTimeoutId] = useState(null);
+  const [currentInput, setCurrentInput] = useState("");
+
   const [activePage, setActivePage] = useState("carsPage");
   const [pickup, setPickup] = useState("");
   const [end, setEnd] = useState("");
@@ -38,6 +47,125 @@ const BookingTraditional = () => {
   const routingControlRef = useRef(null);
   const navigate = useNavigate();
   const { theme } = useContext(CartContext);
+
+  const [query, setQuery] = useState("");
+  const [location, setLocation] = useState(null);
+  const [images, setImages] = useState([]);
+
+  const [popup, setPopup] = useState(false);
+
+  // gợi ý search start
+
+  const handleSearch = async (value, inputField) => {
+    setCurrentInput(inputField); // Cập nhật trường hiện tại đang nhập
+    if (value.length > 3) {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      const newTimeoutId = setTimeout(async () => {
+        try {
+          const results = await provider.search({ query: value });
+          console.log("Results:", results);
+          setSuggestions(results);
+        } catch (error) {
+          console.log("error >>> ", error);
+        }
+      }, 500);
+      setTimeoutId(newTimeoutId);
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  const selectSuggestion = (result) => {
+    setPickup((prevState) => ({
+      ...prevState,
+      [currentInput]: result.label, // Sử dụng currentInput để biết trường nào cần được cập nhật
+    }));
+    setSuggestions([]); // Xóa danh sách gợi ý sau khi chọn
+  };
+  // gợi ý search end
+
+  const content = (
+    <div className="flex flex-col items-center justify-center w-[400px] h-[200px]">
+      <table className="w-full h-full flex flex-row justify-center items-center">
+        <thead className="">
+          <tr className="flex flex-col">
+            <th className="py-4 px-5 bg-gray-200 text-left">Name Customer</th>
+            <th className="py-4 px-5 bg-gray-200 text-left">Name Driver</th>
+            <th className="py-4 px-5 bg-gray-200 text-left">Amount</th>
+          </tr>
+        </thead>
+        <tbody className="">
+          <tr className="flex flex-col">
+            <td className="py-4 px-5 border-b">John Doe</td>
+            <td className="py-4 px-5 border-b">Jane Smith</td>
+            <td className="py-4 px-5 border-b">$50.00</td>
+          </tr>
+        </tbody>
+      </table>
+      <Button
+        className="text-[#FFFFFF] bg-[#FF5F00] mt-2"
+        onClick={() => handlePayment()}
+      >
+        Payment
+      </Button>
+    </div>
+  );
+
+  const searchLocation = async () => {
+    if (!query) return;
+    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(
+      query
+    )}`;
+
+    try {
+      console.log("Fetching location data from URL:", url);
+      let response = await fetch(url);
+      if (!response.ok) {
+        toast.error("Can't Fetching location data");
+      }
+      let data = await response.json();
+      console.log("Received data:", data);
+
+      if (data.length > 0) {
+        var firstResult = data[0];
+        var lat = firstResult.lat;
+        var lon = firstResult.lon;
+        setLocation({
+          display_name: firstResult.display_name,
+          lat: lat,
+          lon: lon,
+        });
+
+        fetchUnsplashImages(firstResult.display_name.split(",")[0]);
+      } else {
+        console.log("Location not found");
+      }
+    } catch (error) {
+      console.error("Error fetching location:", error);
+    }
+  };
+
+  const fetchUnsplashImages = async (query) => {
+    const accessKey = "ZwUHbJbDkzu35eG50ygf_IE8q5x9_va0sSG2jyZh79w";
+    const url = `https://api.unsplash.com/search/photos?query=${encodeURIComponent(
+      query
+    )}&client_id=${accessKey}`;
+    console.log("Fetching images from:", url);
+    try {
+      let response = await fetch(url);
+      if (!response.ok) {
+        toast.error("cant not fetching image");
+      }
+      let data = await response.json();
+      console.log("Received image data:", data);
+      setImages(data.results.map((result) => result.urls.small));
+    } catch (error) {
+      console.error("Error fetching images:", error);
+      setImages([]);
+    }
+  };
 
   useEffect(() => {
     return () => {
@@ -88,12 +216,13 @@ const BookingTraditional = () => {
   const handleRouteFound = (summary) => {
     const distance = (summary.totalDistance / 1000).toFixed(2) + " km";
     const time = (summary.totalTime / 60).toFixed(2) + " minutes";
-    setRouteInfo(`Distance: ${distance}, Time: ${time}`);
+    setRouteInfo({Distance: distance, Time: time});
   };
 
   const handleBooking = (e) => {
     e.preventDefault();
     setIsEdit(!isEdit);
+    searchLocation();
   };
 
   const geocodeAddress = (address, callback) => {
@@ -115,19 +244,32 @@ const BookingTraditional = () => {
 
   const handleSummit = async (e) => {
     e.preventDefault();
+    setPopup(!popup);
     const newInvoice = {
       startPoint: pickup,
       endPoint: end,
-      timeStart: timeCar
+      timeStart: timeCar,
     };
     try {
-      console.log("add invoice"+ newInvoice);
-      const data = await addInvoiceAndTransaction(newInvoice,localStorage.getItem("token"));
+      console.log("add invoice" + newInvoice);
+      const data = await addInvoiceAndTransaction(
+        newInvoice,
+        localStorage.getItem("token")
+      );
       return data;
     } catch (error) {
       console.error(error);
     }
   };
+  const handleEnd = (e) => {
+    setEnd(e.target.value);
+    setQuery(e.target.value);
+  };
+  //TODO
+  const handlePayment = () => {
+    console.log("dkslmk");
+  };
+  //TODO
 
   return (
     <>
@@ -140,14 +282,7 @@ const BookingTraditional = () => {
         }}
       >
         <div>
-          <div className="ml-5">
-            <div className="flex flex-row w-[1120px] justify-between  items-center">
-              <div className="">
-                <img src={images} width={"102px"} alt="logo" className=""></img>
-              </div>
-            </div>
-          </div>
-          <div className="flex justify-center w-[1150px]">
+          <div className="flex  mt-5 justify-center w-[1150px]">
             <div className="flex flex-col items-center  rounded-[20px] w-[1120px] h-[200px] bg-[#FFFFFF] justify-center">
               <div className="flex flex-row justify-start w-full gap-5 mb-10 ml-[22px]">
                 <div
@@ -181,6 +316,7 @@ const BookingTraditional = () => {
                   <label className="font-Roboto font-bold">Pickup</label>
                   <div className="flex flex-row w-[216px] items-center h-[52px] gap-5 rounded-md border-solid border-[#D9D9D9] border-[1px] bg-slate-50 px-[0.75rem]">
                     <input
+                      value={pickup}
                       className="rounded-md w-[200px] h-[40px]"
                       id="start-input"
                       placeholder="Enter your pickup address"
@@ -190,6 +326,19 @@ const BookingTraditional = () => {
                         geocodeAddress(address, setStartPoint);
                       }}
                     />
+                    {suggestions.length > 0 && currentInput === "pickup" && (
+                      <ul className="absolute top-full left-0 mt-1 w-[600px] bg-white shadow-lg max-h-60 overflow-auto z-30 rounded-md">
+                        {suggestions.map((result) => (
+                          <li
+                            key={result.x + result.y}
+                            onClick={() => selectSuggestion(result)}
+                            className="p-2 hover:bg-gray-100 cursor-pointer"
+                          >
+                            {result.label}
+                          </li>
+                        ))}
+                      </ul>
+                    )}
                   </div>
                 </div>
                 <div className="flex flex-col">
@@ -198,8 +347,9 @@ const BookingTraditional = () => {
                     <input
                       className="rounded-md w-[200px] h-[40px] border-[#D9D9D9]"
                       id="end-input"
+                      value={query}
                       placeholder="Enter your return address"
-                      onChange={(e) => setEnd(e.target.value)}
+                      onChange={handleEnd}
                       onBlur={(e) => {
                         const address = e.target.value;
                         geocodeAddress(address, setEndPoint);
@@ -265,15 +415,13 @@ const BookingTraditional = () => {
           <div className="absolute inset-0 bg-black opacity-50 z-40"></div>
           <div className="relative z-50 flex flex-row justify-center w-[1300px] h-[650px] bg-black bg-opacity-75 rounded-lg">
             <div className="flex flex-col bg-white border-2 border-black p-4 shadow-3d">
-              <IoIosCloseCircle
-                onClick={() => setIsEdit(!isEdit)}
-                className="text-red-500 cursor-pointer text-[30px] transition duration-300 transform hover:rotate-90 hover:scale-110 mr-2"
-              />
-              <div className="max-w-64">
+              <div className="max-w-70">
+                <IoIosCloseCircle
+                  onClick={() => setIsEdit(!isEdit)}
+                  className="text-red-500 cursor-pointer text-[30px] transition duration-300 transform hover:rotate-90 hover:scale-110 mr-2"
+                />
+                {/* <PopUpContai /> */}
                 <div>
-                  <h4 className="text-white text-3xl font-bold mb-2">
-                    Invoice
-                  </h4>
                   <p className="text-[#999999] mb-1">
                     Pickup Location: {pickup}
                   </p>
@@ -284,13 +432,31 @@ const BookingTraditional = () => {
                   </p>
                 </div>
                 <div>
-                  <button
-                    className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-xs md:text-sm lg:text-base font-medium bg-[#FF5F00] focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 transition-all relative text-white border-2 z-10 border-white h-10 px-4 py-2 w-full shadow-3d"
-                    type="submit"
-                    onClick={(e) => handleSummit(e)}
-                  >
-                    Confirm
-                  </button>
+                  {location && (
+                    <>
+                      <div className="grid grid-cols-3 justify-center">
+                        {images.map((img, index) => (
+                          <img
+                            key={index}
+                            src={img}
+                            alt={`Image of ${location.display_name}`}
+                            className="m-2 w-[100px] h-[80px] object-cover"
+                          />
+                        ))}
+                      </div>
+                    </>
+                  )}
+                </div>
+                <div>
+                  <Popover content={content} title="Popup" trigger="click">
+                    <button
+                      className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-xs md:text-sm lg:text-base font-medium bg-[#FF5F00] focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 transition-all relative text-white border-2 z-10 border-white h-10 px-4 py-2 w-full shadow-3d"
+                      type="submit"
+                      onClick={(e) => handleSummit(e)}
+                    >
+                      Confirm
+                    </button>
+                  </Popover>
                 </div>
               </div>
             </div>
@@ -334,6 +500,7 @@ const BookingTraditional = () => {
                 />
               )}
             </MapContainer>
+            {/* {popup && <PopUpContai />} */}
           </div>
         </div>
       )}
