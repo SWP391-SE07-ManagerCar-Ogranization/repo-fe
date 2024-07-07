c5863852476541022d0b82fab9a0dfc52eecb16c
PC <muoivo.051064@gmail.com>
2024-07-08 00:18:01 +0700

    update separate working status and group working

--- project/src/routes/DriverRoutes.js
+++ project/src/routes/DriverRoutes.js
@@ -2,6 +2,7 @@ import React from "react";
 import ProtectedRoute from "./ProtectedRoutes";
 import WorkingPage from "../component/driver/WorkingPage";
 import TrackRevenuePage from "../component/driver/TrackRevenuePage";
+import GroupWorkingPage from "../component/driver/GroupWorkingPage";
 
 const DriverRoutes = () => {
   return {
@@ -11,6 +12,14 @@ const DriverRoutes = () => {
         element: (
           <ProtectedRoute roles={["DRIVER"]}>
             <WorkingPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/group-working-page",
+        element: (
+          <ProtectedRoute roles={["DRIVER"]}>
+            <GroupWorkingPage />
           </ProtectedRoute>
         ),
       },
